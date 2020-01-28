710cfefc8212b6bae8891fdd945f8051b767ec1c
miya10kei <miya10kei@gmail.com>
2020-01-28 17:29:22 +0900

    polish

--- background.js
+++ background.js
@@ -1,85 +1,33 @@
-const FAVICON_ID = "tab-bar-extention";
-const INVALID_PREFIX = ["chrome://", "chrome-search://"];
-const EN_NUM = [
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine"
-];
-
+// ---------------------------
+// --- event listenrの設定 ---
+// ---------------------------
 chrome.runtime.onMessage.addListener(msg => {
-  if (msg.event === "keydown") {
-    operationTabs(showNumber);
-  } else if (msg.event === "keyup") {
-    operationTabs(hiddenNumber);
+  if (msg.event === "change") {
+    operateTabs(sendChangeFaviconEvent);
+  } else {
+    operateTabs(sendRevertFaviconEvent);
   }
 });
 
-function operationTabs(callback) {
+// ----------------
+// --- function ---
+// ----------------
+function sendChangeFaviconEvent(tabId, tabIndex) {
+  chrome.tabs.sendMessage(tabId, { type: "changeFavicon", index: tabIndex });
+}
+
+function sendRevertFaviconEvent(tabId) {
+  chrome.tabs.sendMessage(tabId, { type: "revertFavicon" });
+}
+
+function operateTabs(callback) {
   chrome.tabs.query({ currentWindow: true }, tabs => {
     tabs
-      .filter(tab => isValid(tab.url))
+      // 10以上はショートカットでの移動ができないためfaviconを変えない
+      .filter(tab => tab.index < 10)
       .forEach(tab => {
-        const code = callback(tab.index);
-        if (code) {
-          chrome.tabs.executeScript(tab.id, { code: code }, () => {
-            const err = chrome.runtime.lastError;
-            if (err && err.message) {
-              if (!err.message.startsWith("Cannot access contents of")) {
-                console.error(JSON.stringify(err));
-              }
-            }
-          });
-        }
+        callback(tab.id, tab.index + 1);
       });
   });
 }
 
-function isValid(url) {
-  for (let i = 0; i < INVALID_PREFIX.length; i++) {
-    if (!url || url.startsWith(INVALID_PREFIX[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function getUrl(index) {
-  if (index >= 1 && index <= 9) {
-    return `https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/microsoft/209/keycap-digit-${EN_NUM[index]}_3${index}-fe0f-20e3.png`;
-  }
-  return null;
-}
-
-function showNumber(index) {
-  const url = getUrl(index);
-  if (!url) return null;
-
-  return `
-    (function() {
-      const favicon = document.getElementById("${FAVICON_ID}")
-      if (favicon) favicon.remove()
-
-      const link = document.createElement("link");
-      link.id = "${FAVICON_ID}";
-      link.setAttribute("rel", "shortcut icon");
-      link.setAttribute("href", "${url}");
-      document.getElementsByTagName("head")[0].appendChild(link);
-    }())
-  `;
-}
-
-function hiddenNumber() {
-  return `
-  (function() {
-    const favicon = document.getElementById("${FAVICON_ID}")
-    if (favicon) favicon.remove()
-  }())
-  `;
-}
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,71 @@
-window.addEventListener("keydown", function(e) {
-  if (e.key === "Meta") chrome.runtime.sendMessage({ event: "keydown" });
-});
+// TODO  Extension context invalidated.の解決
 
-window.addEventListener("keyup", function(e) {
-  if (e.key === "Meta") chrome.runtime.sendMessage({ event: "keyup" });
+// hrefに"data" URL scheme を設定している場合、
+// loadを待たないと値が取得できなかったの loadのタイミングで実行する
+window.addEventListener("load", () => {
+  const links = [...document.querySelectorAll("link[rel~='icon']")];
+
+  // faviconが未設定の場合は、デフォルトファビコンを設定する
+  if (!links.length) {
+    // TODO default faviconの存在確認
+    const link = createLink("/favicon.ico");
+    document.getElementsByTagName("head")[0].append(link);
+    links.push(link);
+  }
+  const originalUrl = links.map(link => link.getAttribute("href"));
+
+  // ---------------------------
+  // --- event listenrの設定 ---
+  // ---------------------------
+  document.addEventListener("keydown", event => {
+    if (event.key === "Meta") {
+      // TODO keyの設定
+      sendChangeMsg();
+    }
+  });
+  document.addEventListener("keyup", event => {
+    if (event.key === "Meta") {
+      // TODO keyの設定
+      sendRevertMsg();
+    }
+  });
+
+  window.addEventListener("unload", sendChangeMsg);
+  window.addEventListener("blur", sendRevertMsg);
+
+  chrome.runtime.onMessage.addListener(message => {
+    if (message.type === "changeFavicon") {
+      changeFavicon(message.index);
+    } else {
+      revertFavicon();
+    }
+  });
+
+  // ----------------
+  // --- function ---
+  // ----------------
+  function sendChangeMsg() {
+    chrome.runtime.sendMessage({ event: "change" });
+  }
+
+  function sendRevertMsg() {
+    chrome.runtime.sendMessage({ event: "revert" });
+  }
+
+  function createLink(url) {
+    const link = document.createElement("link");
+    link.setAttribute("rel", "icon");
+    link.setAttribute("href", url);
+    return link;
+  }
+
+  function changeFavicon(index) {
+    // extentionのfaviconを取得
+    const url = chrome.runtime.getURL(`images/favicon-${index}.ico`);
+    links.forEach(link => link.setAttribute("href", url));
+  }
+
+  function revertFavicon() {
+    links.forEach((link, i) => link.setAttribute("href", originalUrl[i]));
+  }
 });
